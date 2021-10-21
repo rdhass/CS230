a304f9a84d5e5c155ae4c3c9fe9299812db5b89e
Ryan Hass <ryanhass@stanford.edu>
2021-10-21 13:21:27 -0700

    ddx and ddy have bugs

---
+++ tests/test_derivatives.py
@@ -0,0 +1,76 @@
+import sys
+sys.path.insert(0,'/Users/ryanhass/Documents/MATLAB/CS_230/Final_project/utilities')
+from derivative_operators import ddx, ddy
+import numpy as np
+
+def setup_domain(A,B,C,nx,ny,nz):
+    coefs = [A, B, C]
+    Lx, Ly, Lz = tuple([2.*np.pi*i for i in coefs])
+    nxf, nyf, nzf = np.float64((nx,ny,nz))
+    dx, dy, dz = (Lx/nxf, Ly/nyf, Lz/nzf)
+    
+    x = np.arange(0.,Lx, dx)
+    y = np.arange(0.,Ly, dy)
+    z = np.arange(0.,Lz, dz)
+
+    xmesh, ymesh, zmesh = np.meshgrid(x, x, x, indexing='ij')
+
+    return dx, dy, dz, xmesh, ymesh, zmesh
+
+def test_ddx(A,B,C,nx,ny,nz):
+    # Define spatial domain
+    dx, dy, dz, xmesh, ymesh, zmesh = setup_domain(A,B,C,nx,ny,nz)
+
+    xcos = np.cos(xmesh)
+    xsin = np.sin(xmesh)
+    ycos = np.cos(ymesh)
+    ysin = np.sin(ymesh)
+    zcos = np.cos(zmesh)
+
+    xzcos = np.multiply(xcos,zcos)
+    yzcos = np.multiply(ycos,zcos)
+
+    f = np.multiply(xcos,yzcos)
+
+    dfdx = ddx(f,dx)
+    dfdx_true = np.multiply(-1.*xsin,yzcos)
+    
+    dfdy = ddy(f,dy)
+    dfdy_true = np.multiply(-1.*ysin,xzcos)
+    
+    comparex = dfdx - dfdx_true
+    comparey = dfdy - dfdy_true
+    assert np.amax(comparex) < 1.e-12, "ddx test 1 FAILED. max difference = {}".\
+            format(np.amax(comparex))
+    print("ddx test 1 PASSED!")
+    assert np.amax(comparey) < 1.e-12, "ddy test 1 FAILED. max difference = {}".\
+            format(np.amax(comparey))
+    print("ddy test 1 PASSED!")
+    
+    xsinzcos = np.multiply(xsin,zcos)
+    g = np.multiply(xsin,yzcos)
+    dgdx = ddx(g,dx)
+    dgdx_true = np.multiply(xcos,yzcos)
+    
+    dgdy = ddy(g,dy)
+    dgdy_true = np.multiply(-1.*ysin,xsinzcos)
+    
+    comparex = dgdx - dgdx_true
+    comparey = dgdy - dgdy_true
+    assert np.amax(comparex) < 1.e-12, "ddx test 2 FAILED. max difference = {}".\
+            format(np.amax(comparex))
+    print("ddx test 2 PASSED!")
+    assert np.amax(comparey) < 1.e-12, "ddy test 2 FAILED. max difference = {}".\
+            format(np.amax(comparey))
+    print("ddy test 2 PASSED!")
+    return
+
+if len(sys.argv) < 7:
+    print("Usage:")
+    print*("  python3 test_derivatives.py <Lx_on2pi> <Ly_on2pi> <Lz_on2pi> <nx> <ny> <nz>")
+    sys.exit()
+
+Lx_on2pi, Ly_on2pi, Lz_on2pi, nx, ny, nz = (sys.argv[1], sys.argv[2], sys.argv[3],\
+        sys.argv[4], sys.argv[5], sys.argv[6])
+Lx_on2pi, Ly_on2pi, Lz_on2pi = np.float64((Lx_on2pi, Ly_on2pi, Lz_on2pi))
+test_ddx(Lx_on2pi,Ly_on2pi,Lz_on2pi,nx,ny,nz)
--- utilities/derivative_operators.py
+++ utilities/derivative_operators.py
@@ -2,23 +2,19 @@ import numpy as np
 import numpy.matlib as mat
 from scipy.fft import fft, ifft, fftshift
 
-def __Fourier_coll__(f,dim,nx,ny,nz,h):
-    if dim == 0:
-        N = nx
-        Nrep = ny
-    elif dim == 1:
-        N = ny
-        Nrep = nx
+def __Fourier_coll__(f,dim,h):
+    nx, ny, nz = np.shape(f)
+    N = [nx,ny]
 
-    Nf = np.float64(N)
+    Nf = np.float64(N[dim])
     kvec = fftshift( np.multiply( np.arange(-Nf/2., Nf/2., 1., \
             dtype = np.float64), 2.*np.pi/(Nf*h) ) )
     if dim == 0:
         kvec = kvec.reshape(kvec.size,1)
-        kmat = mat.repmat(kvec,1,Nrep)
+        kmat = mat.repmat(kvec,1,N[1])
     elif dim == 1:
         kvec = kvec.reshape(1,kvec.size)
-        kmat = mat.repmat(kvec,Nrep,1)
+        kmat = mat.repmat(kvec,N[0],1)
 
     fhat = fft(f, axis = dim)
     for kk in range(nz):
@@ -27,11 +23,21 @@ def __Fourier_coll__(f,dim,nx,ny,nz,h):
     return fprime
 
 def ddx(f,dx):
-    nx, ny, nz = np.shape(f)
-    dfdx = __Fourier_coll__(f,0,nx,ny,nz,dx)
+    dfdx = __Fourier_coll__(f,0,dx)
     return dfdx
 
 def ddy(f,dy):
+    #dfdy = __Fourier_coll__(f,1,dy)
     nx, ny, nz = np.shape(f)
-    dfdy = __Fourier_coll__(f,1,nx,ny,nz,dy)
+
+    Nf = np.float64(ny)
+    k2 = fftshift( np.multiply( np.arange(-Nf/2., Nf/2., 1., \
+            dtype = np.float64), 2.*np.pi/(Nf*dy) ) )
+    k2 = k2.reshape(1,k2.size)
+    ky = mat.repmat(k2,nx,1)
+
+    fhat = fft(f, axis = 1)
+    for kk in range(nz):
+        fhat[:,:,kk] = np.multiply(1j*ky,fhat[:,:,kk])
+    dfdy = np.real(ifft(fhat,axis = 1))
     return dfdy
